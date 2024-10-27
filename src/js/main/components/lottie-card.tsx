fd6047066ae853ba992a56c27b006a77869d9de4
Ted Simonian <tedsimonian@gmail.com>
2024-10-27 16:57:18 +0900

    added copy json button to lottie featured animations

--- src/js/main/components/lottie-card.tsx
+++ src/js/main/components/lottie-card.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { DotLottie, DotLottieReact } from "@lottiefiles/dotlottie-react";
-import { Download, Heart, Loader2 } from "lucide-react";
+import { Check, Copy, Download, Heart, Loader2 } from "lucide-react";
 
 import {
   Card,
@@ -18,6 +18,7 @@ import {
 import { FeaturedPublicAnimationsQuery } from "../../lib/__generated__/graphql";
 import { ArrayItem } from "../../types";
 import { cn, getReadableTextColor } from "../../../shared/utils";
+import { Button } from "../../../shared/components/ui/button";
 
 type LottieCardProps = {
   animation: ArrayItem<
@@ -27,14 +28,27 @@ type LottieCardProps = {
 
 export const LottieCard: React.FC<LottieCardProps> = ({ animation }) => {
   const [dotLottie, setDotLottie] = useState<DotLottie | null>(null);
+  const [isCopied, setIsCopied] = useState(false);
 
   const dotLottieRefCallback = (dotLottie: DotLottie) => {
     setDotLottie(dotLottie);
   };
 
+  const handleClipboardCopy = () => {
+    if (animation.node.jsonUrl) {
+      fetch(animation.node.jsonUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          navigator.clipboard.writeText(JSON.stringify(data, null, 2));
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false), 2000);
+        });
+    }
+  };
+
   return (
     <Card
-      className="w-full h-[320px] flex flex-col transition-shadow hover:shadow-lg overflow-hidden"
+      className="w-full h-full flex flex-col transition-shadow hover:shadow-lg overflow-hidden"
       style={{ backgroundColor: animation.node.bgColor || "white" }}
     >
       <CardHeader className="p-4 flex-grow justify-center">
@@ -69,11 +83,15 @@ export const LottieCard: React.FC<LottieCardProps> = ({ animation }) => {
       </CardContent>
       <CardFooter
         className={cn(
-          "p-4 pt-0 flex justify-between mt-auto items-start flex-wrap",
-          getReadableTextColor(animation.node.bgColor || "white")
+          "p-4 pt-0 flex justify-between mt-auto items-start flex-wrap "
         )}
       >
-        <div className="flex items-center space-x-2 mb-2">
+        <div
+          className={cn(
+            "flex items-center space-x-2 mb-2",
+            getReadableTextColor(animation.node.bgColor || "white")
+          )}
+        >
           <Avatar className="w-6 h-6">
             <AvatarImage
               src={animation.node.createdBy?.avatarUrl}
@@ -83,24 +101,32 @@ export const LottieCard: React.FC<LottieCardProps> = ({ animation }) => {
               {animation.node.createdBy?.username}
             </AvatarFallback>
           </Avatar>
-          <span
+          <span className="text-sm">{animation.node.createdBy?.username}</span>
+        </div>
+        <div className="flex items-center space-x-2 justify-between w-full">
+          <div
             className={cn(
-              "text-sm",
+              "flex items-center space-x-2",
               getReadableTextColor(animation.node.bgColor || "white")
             )}
           >
-            {animation.node.createdBy?.username}
-          </span>
-        </div>
-        <div className="flex items-center space-x-2">
-          <div className="flex items-center space-x-1">
-            <Download className="w-4 h-4" />
-            <span className="text-sm">{animation.node.downloads}</span>
+            <div className="flex items-center space-x-1">
+              <Download className="w-4 h-4" />
+              <span className="text-sm">{animation.node.downloads}</span>
+            </div>
+            <div className="flex items-center space-x-1">
+              <Heart className="w-4 h-4" />
+              <span className="text-sm">{animation.node.likesCount}</span>
+            </div>
           </div>
-          <div className="flex items-center space-x-1">
-            <Heart className="w-4 h-4" />
-            <span className="text-sm">{animation.node.likesCount}</span>
-          </div>
+
+          <Button variant="outline" size="icon" onClick={handleClipboardCopy}>
+            {isCopied ? (
+              <Check className="h-4 w-4" />
+            ) : (
+              <Copy className="h-4 w-4" />
+            )}
+          </Button>
         </div>
       </CardFooter>
     </Card>
